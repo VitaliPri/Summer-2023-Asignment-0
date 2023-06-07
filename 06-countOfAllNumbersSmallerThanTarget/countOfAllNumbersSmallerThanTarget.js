51a8c6dc01c960dc1faf942106a95ccc9d8d0e76
v <v@vs-iMac.local>
2023-06-07 17:49:05 -0400

    06

--- 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
+++ 06-countOfAllNumbersSmallerThanTarget/countOfAllNumbersSmallerThanTarget.js
@@ -1,5 +1,9 @@
 function countOfAllNumbersSmallerThanTarget(nums, target) {
-  // Insert code here;
+  let newArray = [];
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] < target) newArray.push(nums[i]);
+  }
+  return newArray.length;
 }
 
 // Do not edit this line;
