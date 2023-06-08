684adb5c4e75dbb5a2afa84d1d9cd530da4b65c5
v <v@vs-iMac.local>
2023-06-07 21:23:51 -0400

    09

--- 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
+++ 09-sumOfAllOddNumbers/sumOfAllOddNumbers.js
@@ -1,5 +1,15 @@
 function sumOfAllOddNumbers(nums) {
-  // Insert code here;
+  let count = 0;
+  for (let i = 0; i < nums.length; i++) {
+    if (nums[i] % 2 == 1) count++;
+  }
+  return count;
+
+  // let countOdd = 0;
+  // let odd = nums.forEach(function (i) {
+  //   if (nums[i] % 2 == 1) countOdd++;
+  // });
+  // console.log(countOdd);
 }
 
 // Do not edit this line;
