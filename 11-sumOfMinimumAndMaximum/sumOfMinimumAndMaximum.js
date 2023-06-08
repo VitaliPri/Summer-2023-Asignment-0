0dbac8b0181ffe3d8832faa0e373b8781cf3ec49
v <v@vs-iMac.local>
2023-06-08 10:59:53 -0400

    11

--- 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
+++ 11-sumOfMinimumAndMaximum/sumOfMinimumAndMaximum.js
@@ -1,5 +1,13 @@
 function sumOfMinimumAndMaximum(nums) {
-  // Insert code here;
+  let maximum = -Infinity;
+  let minimum = Infinity;
+
+  for (let number of nums) {
+    if (number > maximum) maximum = number;
+    if (number < minimum) minimum = number;
+  }
+
+  return maximum + minimum;
 }
 
 // Do not edit this line;
