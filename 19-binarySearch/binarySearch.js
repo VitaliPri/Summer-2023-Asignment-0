1ca3c0c39d8ea4f32856619d868cd6e9198148c7
v <v@vs-iMac.local>
2023-06-08 23:12:18 -0400

    19

--- 19-binarySearch/binarySearch.js
+++ 19-binarySearch/binarySearch.js
@@ -4,7 +4,19 @@ class MySolution {
   }
 
   binarySearch(nums, target) {
-    // Insert code here;
+    let arr = Array.from(nums);
+    let mid = Math.floor(arr.length / 2);
+    let first = 0;
+    let last = arr.length - 1;
+
+    if (target == arr[mid]) return true;
+    else if (target < arr[mid]) {
+      arr = arr.splice(first, mid);
+      return this.binarySearch(arr, target);
+    } else if (target > arr[mid]) {
+      arr = arr.splice(mid + 1, last);
+      return this.binarySearch(arr, target);
+    } else return false;
   }
 }
 
